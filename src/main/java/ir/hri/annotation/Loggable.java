b9ff9e67c72d5744f0128cf3d9e5c1aa6b13d2d8
HR <ireh.hr@gmail.com>
2016-12-01 11:22:18 +0330

    Custom Annotation

---
+++ src/main/java/ir/hri/annotation/Loggable.java
@@ -0,0 +1,12 @@
+package ir.hri.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Loggable {
+    boolean recordEvent() default false;
+}
